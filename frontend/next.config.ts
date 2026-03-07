18f8e3afc429a91ebfb83d23e05156a50fbed624
nexus-x86 <60083031+nexus-x86@users.noreply.github.com>
2026-03-07 16:48:57 -0500

    vercel support pls

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,16 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  rewrites: async () => {
+    return [
+      {
+        source: "/api/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:8000/api/:path*"
+            : "/api/",
+      },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
